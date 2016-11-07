d5fc298914bed51cd63a90707355aff2fc77774d
avinashraz <avinashaninthaneni@gmail.com>
2016-11-07 10:53:05 +0530

    Encapsulation
    Encapsulation Class For Employee Details.

---
+++ src/Emp.java
@@ -0,0 +1,14 @@
+
+public class Emp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee emp = new Employee(10,"Avinash","Chennai");
+System.out.println("ID - "+emp.getId());
+System.out.println("NAME - "+ emp.getName());
+System.out.println("ADDRESS - "+emp.getAddress());
+
+
+	}
+
+}
